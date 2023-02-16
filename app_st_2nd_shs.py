e2b55c82b223541470b778fbb8c1d962fddefb21
SSong-SSong <gksthf3609@gmail.com>
2023-02-16 16:27:45 +0900

    new

--- app_st_2nd_shs.py
+++ app_st_2nd_shs.py
@@ -8,13 +8,13 @@ import plotly.express as px
 import io
 
 with st.sidebar:
-    choose = option_menu("App Gallery", ["About", "Photo Editing", "Project Planning"],
+    choose = option_menu("스마트도시연구본부", ["Green Remodeling", "Metro", "GHG Emission"],
                          icons=['house', 'camera fill', 'kanban'],
-                         menu_icon="app-indicator", default_index=0,
+                         menu_icon="bi bi-list-columns-reverse", default_index=0,
                          styles={
         "container": {"padding": "5!important", "background-color": "#6e6a6a"},
         "icon": {"color": "orange", "font-size": "25px"}, 
         "nav-link": {"font-size": "16px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
-        "nav-link-selected": {"background-color": "#02ab21"},
+        "nav-link-selected": {"background-color": "#0a0a0a"},
     }
     )
